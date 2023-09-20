159d916013f03a3b6a65e57d2eb6ac138038d378
Kristof Beyls <kristof.beyls@arm.com>
2023-09-20 11:33:51 +0200

    [GadgetScanner/pacret] Add test that exercises the dataflow confluence operator.

--- bolt/test/gadget-scanner/gs-pacret-multi-bb.s
+++ bolt/test/gadget-scanner/gs-pacret-multi-bb.s
@@ -2,7 +2,7 @@
 // RUN: llvm-bolt-gadget-scanner %t.exe 2>&1 | FileCheck -check-prefix=CHECK --allow-empty %s
 
 
-/// Verify that we can also detect gadgets across basic blocks
+// Verify that we can also detect gadgets across basic blocks
 
         .globl f_crossbb1
         .type   f_crossbb1,@function
@@ -18,6 +18,24 @@ f_crossbb1:
 // CHECK: GS-PACRET: non-protected ret found in function f_crossbb1, basic block .L{{[^,]+}}, at address
 // CHECK:     00000014:   ret     x30 # pacret-gadget: pac-ret-gadget<Ret:MCInstBBRef<BB:
 
+// A test that checks that the dataflow state tracking across when merging BBs
+// seems to work:
+        .globl f_mergebb1
+        .type   f_mergebb1,@function
+f_mergebb1:
+        hint    #25
+2:
+        stp     x29, x30, [sp, #-16]!
+        ldp     x29, x30, [sp], #16
+        sub     x0, x0, #1
+        cbnz    x0, 1f
+        autiasp
+        b       2b
+1:
+        ret
+        .size f_mergebb1, .-f_mergebb1
+// CHECK: GS-PACRET: non-protected ret found in function f_mergebb1, basic block .L{{[^,]+}}, at address
+// CHECK:     0000001c:   ret     x30 # pacret-gadget: pac-ret-gadget<Ret:MCInstBBRef<BB:
 
-/// TODO: also verify that false negatives exist in across-BB gadgets in functions
-/// for which bolt cannot reconstruct the call graph.
+// TODO: also verify that false negatives exist in across-BB gadgets in functions
+// for which bolt cannot reconstruct the call graph.
